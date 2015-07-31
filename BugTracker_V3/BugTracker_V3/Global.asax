2250a79b09b9567d1260bf92ca5ed9e8aef4ba4b
Ray Goodwin <rgoodwin16@outlook.com>
2015-07-31 06:36:45 -0400

    7/31/15
    First Commit

---
+++ BugTracker_V3/BugTracker_V3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BugTracker_V3.MvcApplication" Language="C#" %>
---
+++ BugTracker_V3/BugTracker_V3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTracker_V3.Startup))]
+namespace BugTracker_V3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
